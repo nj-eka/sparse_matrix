ccf1c6e5dab869163a261294506aa4a4ce914f53
Jack New <njeka0108@gmail.com>
2023-08-17 19:37:18 +0300

    refactoring

--- src/sparse/matrix.hpp
+++ src/sparse/matrix.hpp
@@ -5,61 +5,56 @@
 #include <ostream>
 #include <utility>
 
-// #include <iostream>
-// #define LOG (std::cout << __PRETTY_FUNCTION__ << std::endl)
-
 namespace sparse {
-
-template <typename T, size_t N_DIMS = 2>
-class Matrix;
 
 namespace details {
 
+template <typename T, size_t N_DIMS = 2>
+struct CellAccessor {
+  virtual T const& get(std::array<size_t, N_DIMS> const&) const = 0;
+  virtual void set(std::array<size_t, N_DIMS> const&, T &&) = 0;
+  virtual void set(std::array<size_t, N_DIMS> const&, const T &) = 0;
+};
+
 template <typename T, size_t N_DIMS, size_t DIM>
 class ShiftIndex {
-  Matrix<T, N_DIMS>* _matrix;
+  CellAccessor<T, N_DIMS>* _cell;
   // todo: try to optimize the index transfer while avoiding some side effects (described in [Sparse Matrix Code Dependence Analysis Simplification at Compile Time])
   std::array<size_t, DIM + 1> _idx;
 public:
-  ShiftIndex(Matrix<T, N_DIMS>* matrix, std::array<size_t, DIM> idx) : _matrix{matrix} {
+  ShiftIndex(CellAccessor<T, N_DIMS>* cell, std::array<size_t, DIM> idx) : _cell{cell} {
     std::copy(std::begin(idx), std::end(idx), std::begin(_idx));
   }
   auto operator[](size_t last_idx) {
     _idx[DIM] = last_idx;
-    return ShiftIndex<T, N_DIMS, DIM+1>(_matrix, _idx);
+    return ShiftIndex<T, N_DIMS, DIM+1>(_cell, _idx);
   }
 };
 
 template <typename T, size_t N_DIMS>
 class ShiftIndex<T, N_DIMS, N_DIMS> {
-  Matrix<T, N_DIMS>* _matrix;
-  std::array<size_t, N_DIMS> _idx;
+  CellAccessor<T, N_DIMS>* _cell;
+  std::array<size_t, N_DIMS> const _idx;
 public:
-  ShiftIndex(Matrix<T, N_DIMS>* matrix, std::array<size_t, N_DIMS> idx) : _matrix{matrix} {
-    std::copy(std::begin(idx), std::end(idx), std::begin(_idx));
-    // LOG;
-  }
+  ShiftIndex(CellAccessor<T, N_DIMS>* cell, std::array<size_t, N_DIMS> const& idx) : _cell{cell}, _idx{idx} {}
   auto& operator=(T &&value){
-    // LOG;
-    _matrix->put(_idx, std::forward<T>(value));
+    _cell->set(_idx, std::forward<T>(value));
     return *this;
   }
   auto& operator=(T const& value){
-    // LOG;
-    _matrix->put(_idx, value);
+    _cell->set(_idx, value);
     return *this;
   }
   operator T() const
   {
-    // LOG;
-    return _matrix->get(_idx);
+    return _cell->get(_idx);
   }
 };
 
 } // namespace details
 
-template <typename T, size_t N_DIMS>
-class Matrix {
+template <typename T, size_t N_DIMS = 2>
+class Matrix: public details::CellAccessor<T, N_DIMS> {
   std::map<std::array<size_t, N_DIMS>, T> _map;
   T const _default;
  public:
@@ -68,31 +63,29 @@ class Matrix {
 
   Matrix(const T& default_value = T{}): _default{default_value} {};
 
-  T const& get(std::array<size_t, N_DIMS> const& idx) const {
+  T const& get(std::array<size_t, N_DIMS> const& idx) const override {
     auto const& it = _map.find(idx);
     if (it == _map.end()) {
       return _default;
     }
     return it->second;
   }
-  void put(std::array<size_t, N_DIMS> const& idx, T &&value) {
+  void set(std::array<size_t, N_DIMS> const& idx, T &&value) override {
     if (value == _default){
       _map.erase(idx);
     } else {
-      // _map[idx] = value; // + 1) T::T(&&) + 2) T& T::operator=(const T&)
-      // _map.emplace(idx, std::forward<T>(value));  // + 1) T::T(&&) + 2) T& T::operator=(const T&)
       // _map.emplace(std::piecewise_construct, std::forward_as_tuple(idx), std::forward_as_tuple(value)); // T::T(const T&)
       // _map.try_emplace(idx, value); // T::T(const T&)
       _map.insert_or_assign(idx, std::forward<T>(value)); // insert = 1) T::T(&&) + 2) T& T::operator=(const T&) assign = T& T::operator=(const T&) + T::T(const T&)
     }
   }
-  void put(std::array<size_t, N_DIMS> const& idx, const T &value) {
-    // LOG;
+  void set(std::array<size_t, N_DIMS> const& idx, const T &value) override {
     if (value == _default)
       _map.erase(idx);
     else
       _map[idx] = value;
-  }  
+  }
+
   auto operator[](size_t idx1) {
     std::array<size_t, 1> shiftIndex;
     shiftIndex[0] = idx1;
